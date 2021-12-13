614a1a64ae37cc35b1e6dc822bec287b9ab00528
Xavier Ouimet <xavier.ouimet@leddartech.com>
2021-12-13 17:29:40 -0500

    feat: version for 1.3.0 release

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ from subprocess import CalledProcessError
 
 kwargs = dict(
     name='pioneer_common_gui',
-    version='1.1.1',
+    version='1.3.0',
     author='Leddartech',
     description='Pioneer gui utility package',
     long_description='',
